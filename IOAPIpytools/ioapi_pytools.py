5af9479fd405a42c5e82f354103e466be488c7b3
Timothy W. Hilton <thilton@ucmerced.edu>
2016-02-01 15:47:23 -0800

    implement boundaries_from_csv, dummy_top_bounds
    - these create lateral and top bounds files containing user-specified
      data

--- IOAPIpytools/ioapi_pytools.py
+++ IOAPIpytools/ioapi_pytools.py
@@ -275,6 +275,168 @@ def run_regrid_NN(fname_raw, fname_regridded, fname_griddesc, out_grid_name):
                     shell=True)
 
 
+def boundaries_from_csv(fname_csv,
+                        fname_bdy,
+                        fname_griddesc,
+                        gridname,
+                        nlevs,
+                        file_desc="boundary data"):
+    """Create lateral boundary conditions file from data in CSV file
+
+    Run `M3FAKE
+    <https://www.cmascenter.org/ioapi/documentation/3.1/html/M3FAKE.html>`_
+    to create a boundary condition file from a specified csv file
+    containing the boundaries data.  Models-3 I/O API `"Logical names"
+    <https://www.cmascenter.org/ioapi/documentation/3.1/html/LOGICALS.html>`_
+    are defined internally, allowing the user to specify paths to the
+    physical files to be manipulated.
+
+    Currently only `time-independent
+    <https://www.cmascenter.org/ioapi/documentation/3.1/html/DATETIME.html>`_
+    boundary files are supported.
+
+    Currently can only use the default vertical sigma-P coordinates
+    suggested by M3FAKE.
+
+    ARGS:
+    fname_csv (string): full path to the comma-separated variable
+        data file containing the boundary conditions.
+    fname_bdy (string): full path to the Models-3 I/O API boundaries
+        file to create.  Will be overwritten if the file already exists.
+    fname_griddesc (string): full path to the Models-3 I/O API
+       `GRIDDESC
+       <http://www.cmascenter.org/ioapi/documentation/3.1/html/GRIDS.html#horiz>`_
+       file describing the model grid.
+    gridname (string): name of the grid to create boundaries for (must
+        be an entry in the GRIDDESC file).
+    nlevs (int): number of vertical levels.
+    file_desc (string): string to populate the `FDESC3D
+        <http://www.cmascenter.org/ioapi/documentation/3.1/html/INCLUDE.html#fdesc>`_
+        metadata field in the boundary condition file.  Defaults to
+        "boundary data".
+
+    RETURNS:
+    None
+    """
+
+    os.environ['GRIDDESC'] = fname_griddesc
+    os.environ['OUTFILE'] = fname_bdy
+    os.environ['CSV_FILE'] = fname_csv
+
+    # some netcdf, I/O API routines barf if the output file exists
+    # already -- therefore delete it now
+    delete_if_exists(fname_bdy)
+    cmd = ('m3fake << DONE\n'
+           'Y\n'   #
+           '3\n'   # 3 == boundary file
+           '{gridname}\n'  # name of grid in GRIDDESC
+           '{nlevs}\n'  # number of vertical levels
+           '4\n'  # 4 == hydrostatic sigma-P vertical coords
+           '\n'  # default sigma-P for model top
+           '\n'  # default sigma-P for level 0
+           '\n'  # .
+           '\n'  # .
+           '\n'  # .
+           '\n'  # .
+           '\n'  # .
+           '\n'  # .
+           '\n'  # .
+           '\n'  # .
+           '\n'  # .
+           '\n'  # .
+           '\n'  # .
+           '\n'  # .
+           '\n'  # .
+           '\n'  # .
+           '\n'  # .
+           '\n'  # .
+           '\n'  # .
+           '\n'  # .
+           '\n'  # .
+           '\n'  # .
+           '\n'  # .
+           '\n'  # default sigma-P for level 22
+           '0\n'  # time-independent file
+           '1\n'  # number of variables
+           'CO2_TRACER1\n'  # variable name
+           'ppbv\n'   # variable units
+           '[COS]\n'  # variable description
+           '1\n'  # 1 == type real (as opposed to int double etc)
+           '1\n'  # 1 == fill file with column number
+           #'CSV_FILE\n'  # data file logical name
+           '{file_desc}\n\n'
+           'OUTFILE\n'  # logical name of boundary file
+           'DONE\n'.format(gridname=gridname,
+                           nlevs=nlevs,
+                           file_desc=file_desc))
+
+    subprocess.call(cmd, shell=True)
+
+
+def dummy_top_bounds(fname_bdy,
+                     fname_griddesc,
+                     gridname,
+                     file_desc="boundary data"):
+    """Create lateral boundary conditions file from data in CSV file
+
+    Run `M3FAKE
+    <https://www.cmascenter.org/ioapi/documentation/3.1/html/M3FAKE.html>`_
+    to create a top boundary condition file containing 0.0 at all
+    locations.  Models-3 I/O API `"Logical names"
+    <https://www.cmascenter.org/ioapi/documentation/3.1/html/LOGICALS.html>`_
+    are defined internally, allowing the user to specify paths to the
+    physical files to be manipulated.
+
+    Currently only `time-independent
+    <https://www.cmascenter.org/ioapi/documentation/3.1/html/DATETIME.html>`_
+    boundary files are supported.
+
+    ARGS:
+    fname_bdy (string): full path to the Models-3 I/O API boundaries
+        file to create.  Will be overwritten if the file already exists.
+    fname_griddesc (string): full path to the Models-3 I/O API
+       `GRIDDESC
+       <http://www.cmascenter.org/ioapi/documentation/3.1/html/GRIDS.html#horiz>`_
+       file describing the model grid.
+    gridname (string): name of the grid to create boundaries for (must
+        be an entry in the GRIDDESC file).
+    file_desc (string): string to populate the `FDESC3D
+        <http://www.cmascenter.org/ioapi/documentation/3.1/html/INCLUDE.html#fdesc>`_
+        metadata field in the boundary condition file.  Defaults to
+        "boundary data".
+
+    RETURNS:
+    None
+
+    """
+
+    os.environ['GRIDDESC'] = fname_griddesc
+    os.environ['OUTFILE'] = fname_bdy
+
+    # some netcdf, I/O API routines barf if the output file exists
+    # already -- therefore delete it now
+    delete_if_exists(fname_bdy)
+    cmd = ('m3fake << DONE\n'
+           'Y\n'   #
+           '2\n'   # 3 == gridded file
+           '{gridname}\n'  # name of grid in GRIDDESC
+           '1\n'  # number of vertical levels
+           '0\n'  # time-independent file
+           '1\n'  # number of variables
+           'CO2_TRACER1\n'  # variable name
+           'ppbv\n'   # variable units
+           '[COS]\n'  # variable description
+           '1\n'  # 1 == type real (as opposed to int double etc)
+           '1\n'  # 1 == fill file with column number
+           #'CSV_FILE\n'  # data file logical name
+           '{file_desc}\n\n'
+           'OUTFILE\n'  # logical name of boundary file
+           'DONE\n'.format(gridname=gridname,
+                           file_desc=file_desc))
+
+    subprocess.call(cmd, shell=True)
+
+
 def run_bcwndw(fname_gridded, fname_bdy,
                LOROW=1, HIROW=123,
                LOCOL=1, HICOL=123):
@@ -301,6 +463,8 @@ def run_bcwndw(fname_gridded, fname_bdy,
                     'DONE\n'.format(LOCOL=LOCOL, HICOL=HICOL,
                                     LOROW=LOROW, HIROW=HIROW),
                     shell=True)
+
+
 
 def concat(file1, file2):
     """concatenate file2 to file1 using m3xtract"""
--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'A collection of wrapper functions for the Models-3 I/O API',
+    'description': ('A collection of wrapper functions'
+                    ' for the Models-3 I/O API'),
     'author': 'Timothy W. Hilton',
     'url': 'thilton@ucmerced.edu',
     'download_url': 'thilton@ucmerced.edu',
     'author_email': 'thilton@ucmerced.edu',
     'version': '0.1',
     'install_requires': ['numpy'],
-    'packages': ['ioapi_pytools'],
+    'packages': ['IOAPIpytools'],
     'scripts': [],
     'name': 'IOAPIpytools'
 }
